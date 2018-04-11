cc7976debe62be2e3123f3c2b188ac8cf82f8810
Dean Flint <dean.p.flint@gmail.com>
2018-04-11 13:40:57 +0000

    Add new form fields jquery added and dropdown jquery added.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,30 @@
-// Navbar
 $(document).ready(function(){
+    // Navbar
     $('.sidenav').sidenav();
+    // Dropdown
+    $('select').formSelect();
   });
+
+  
+$("#add_ingr").click(function(){
+  var newInput = $(document.createElement('input'))
+   .attr({
+       id: 'ingr',
+       name: 'ingr',
+       type: "text",
+       "class": "validate"
+   });
+  $("#ingredients").append(newInput);
+});
+
+
+$("#add_step").click(function(){
+  var newInput = $(document.createElement('input'))
+   .attr({
+       id: 'step',
+       name: 'step',
+       type: "text",
+       "class": "validate"
+   });
+  $("#steps").append(newInput);
+});
