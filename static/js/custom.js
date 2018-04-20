bb6be63d15f52935ccb730310dcd9fb5a2de401e
Dean Flint <dean.p.flint@gmail.com>
2018-04-20 13:58:51 +0000

    Tidy up

--- static/js/custom.js
+++ static/js/custom.js
@@ -6,32 +6,44 @@ $(document).ready(function(){
   });
 
 
-var ing_i = 1;
-var step_i = 1;
+//var ing_i = 1;
+//var step_i = 1;
 
 $("#add_ingr").click(function(){
   var newInput = $(document.createElement('input'))
    .attr({
-       id: 'ingr'+ing_i,
+       //id: 'ingr'+ing_i,
        name: 'ingr',
        type: "text",
        "class": "validate ingr"
    });
   $("#ingredients").append(newInput);
-  ing_i++;
+  //ing_i++;
+});
+
+$('#remove_ingr').on('click', function () {
+    if($('#ingredients input').length > 1)
+    {
+        $('#ingredients input:text:last').remove();
+    }
 });
 
 $("#add_step").click(function(){
-  var newInput = $(document.createElement('input'))
+  var newInput = $(document.createElement('textarea'))
    .attr({
-       id: 'step'+step_i,
+       //id: 'step'+step_i,
        name: 'step',
        type: "text",
-       "class": "validate"
+       "class": "materialize-textarea validate"
    });
   $("#steps").append(newInput);
-  step_i++;
+  //step_i++;
 });
 
- 
+$('#remove_step').on('click', function () {
+    if($('#steps input').length > 1)
+    {
+        $('#steps input:text:last').remove();
+    }
+});
 
